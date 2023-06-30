dd78003edd7a6b4270f26d2734a005544f65427e
karimabdrakhmanov <j3t1x0h@gmail.com>
2023-06-30 17:45:41 +0600

    3

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,14 +1,11 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static ThreadSafeSingleton instance;
+    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
     private ThreadSafeSingleton(){}
     public static ThreadSafeSingleton getInstance(){
-        if (instance == null) {
-            instance = new ThreadSafeSingleton();
-        }
+
         return instance;
     }
-    // other useful methods here
     }
 
